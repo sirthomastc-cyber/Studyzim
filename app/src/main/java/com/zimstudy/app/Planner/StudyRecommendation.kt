e83ea8b8cfbb403f7572cdde23b2eb319c59ce6b
sirthomastc-cyber <sirthomastc@gmail.com>
2026-08-16 16:15:10 +0200

    Update StudyRecommendation.kt

--- app/src/main/java/com/zimstudy/app/Planner/StudyRecommendation.kt
+++ app/src/main/java/com/zimstudy/app/Planner/StudyRecommendation.kt
@@ -1,1 +1,14 @@
+package com.zimstudy.app.planner
 
+
+data class StudyRecommendation(
+
+    val subject: String,
+
+    val topic: String,
+
+    val reason: String,
+
+    val durationMinutes: Int
+
+)
