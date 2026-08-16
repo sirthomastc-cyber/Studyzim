142aeff2dc0021211d4e6222983395623bf879ee
sirthomastc-cyber <sirthomastc@gmail.com>
2026-08-16 12:27:05 +0200

    Create StudyContext.kt

---
+++ app/src/main/java/com/zimstudy/app/ai/StudyContext.kt
@@ -0,0 +1,12 @@
+package com.zimstudy.app.ai
+
+
+data class StudyContext(
+
+    val subject: String = "General",
+
+    val level: String = "ZIMSEC O-Level",
+
+    val topic: String = "General Study"
+
+)
