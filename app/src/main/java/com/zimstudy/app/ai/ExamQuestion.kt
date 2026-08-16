4cbeb0b7ce754a8c5ea591f366bfc1172a07c57f
sirthomastc-cyber <sirthomastc@gmail.com>
2026-08-16 12:45:59 +0200

    Create ExamQuestion.kt

---
+++ app/src/main/java/com/zimstudy/app/ai/ExamQuestion.kt
@@ -0,0 +1,18 @@
+package com.zimstudy.app.ai
+
+
+data class ExamQuestion(
+
+    val questionNumber: Int,
+
+    val questionText: String,
+
+    val subject: String,
+
+    val topic: String,
+
+    val marks: Int,
+
+    val difficulty: String
+
+)
